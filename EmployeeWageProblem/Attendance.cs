de8318f7361494a09938ebd8a18a1fdd12022ded
unknown <Pratik@Example.com>
2022-11-07 18:50:34 +0530

    [Pratik]Add:Folder Structure Created

---
+++ EmployeeWageProblem/Attendance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblems
+{
+    public class EmpWage
+    {
+        Random random = new Random();
+        public void Attendance()
+        {
+            int check = random.Next(0, 2);
+            if (check == 0)
+                Console.WriteLine("Employee is Present.");
+            else
+                Console.WriteLine("Employee is absent");
+        }
+    }
+}
---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeWageProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Problems Statement");
+            EmpWage emp = new EmpWage();
+            emp.Attendance();
+        }
+    }
+}
