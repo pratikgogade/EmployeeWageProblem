f7843839343c34810d5144044e9ec065a7392d35
unknown <Pratik@Example.com>
2022-11-07 19:02:56 +0530

    [Pratik]Add:Daily Employee Wage

--- EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -8,6 +8,7 @@ namespace EmployeeWageProblems
 {
     public class EmpWage
     {
+        const int WAGE_PER_HR = 20, FULL_DAY_HR = 8;
         Random random = new Random();
         public void Attendance()
         {
@@ -16,6 +17,15 @@ namespace EmployeeWageProblems
                 Console.WriteLine("Employee is Present.");
             else
                 Console.WriteLine("Employee is absent");
+        }
+
+        public void Wage()
+        {
+            int salary = 0;
+            int check = random.Next(0, 2);
+            if (check == 0)
+                salary = WAGE_PER_HR * FULL_DAY_HR;
+            Console.WriteLine("Wage of the Employee is : " + salary);
         }
     }
 }
--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -9,6 +9,7 @@ namespace EmployeeWageProblems
             Console.WriteLine("Welcome to Employee Wage Problems Statement");
             EmpWage emp = new EmpWage();
             emp.Attendance();
+            emp.Wage();
         }
     }
 }
