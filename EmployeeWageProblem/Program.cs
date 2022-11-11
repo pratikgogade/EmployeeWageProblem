479c1384cba75b022589310e24a318e50c0b4c21
unknown <Pratik@Example.com>
2022-11-11 11:57:18 +0530

    [Pratik]Add:Using Switchcase

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -7,9 +7,27 @@ namespace EmployeeWageProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Problems Statement");
-            EmpWage emp = new EmpWage();
-            emp.Attendance();
-            emp.Wage();
+            Console.WriteLine("Enter your choice what you want to know?"
+                + "\n" + "1. Attendance" + "\n" + "2. Daily Wage" + "\n" + "3. Monthly Wage" + "\n" + "4. Exit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Attendance attend = new Attendance();
+                    attend.attendance();
+                    break;
+                case 2:
+                    FullTimePartTime wage = new FullTimePartTime();
+                    wage.Daily();
+                    break;
+                case 3:
+                    MonthlyEmpWage emp = new MonthlyEmpWage();
+                    emp.MonthlyWage();
+                    break;
+                default:
+                    Console.WriteLine("Try Again");
+                    break;
+            }
         }
     }
 }
